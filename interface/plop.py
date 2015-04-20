2988f0f8263a5e25de4452f2ce2342eb8c57167c
Pierre Baillet <pierre@baillet.name>
2015-04-20 16:45:21 +0200

    On

--- interface/plop.py
+++ interface/plop.py
@@ -8,14 +8,14 @@ class FullFrame(wx.Frame):
     def __init__(self, title):
 
 
-        w = wx.SystemSettings.GetMetric(wx.SYS_SCREEN_X)
-        h = wx.SystemSettings.GetMetric(wx.SYS_SCREEN_Y)
+        w = 320 #  wx.SystemSettings.GetMetric(wx.SYS_SCREEN_X)
+        h = 240 # wx.SystemSettings.GetMetric(wx.SYS_SCREEN_Y)
 
         size = wx.Size(w,h)
-        pos = wx.Point(0,0)
+        pos = wx.Point(100,100)
 
-        wx.Frame.__init__(self, None, wx.ID_ANY, title=title, pos = pos, size = size,
-        style = wx.STAY_ON_TOP | wx.CLIP_CHILDREN )
+        wx.Frame.__init__(self, None, title=title, pos = pos, size = size)
+        # style = wx.STAY_ON_TOP | wx.CLIP_CHILDREN )
 
         panel = wx.Panel(self)
         box = wx.BoxSizer(wx.VERTICAL)
@@ -23,7 +23,14 @@ class FullFrame(wx.Frame):
         button = wx.Button(self, 1, "Lights OFF", size = (w/3, h/3))
         self.Bind(wx.EVT_BUTTON, self.LightsOff, button)
 
-        box.Add(button, 0, wx.ALL, 10)
+        panel.Add(button, 0, wx.ALL, 10)
+
+
+        button = wx.Button(self, 1, "Lights ON", size = (w/3, h/3))
+        self.Bind(wx.EVT_BUTTON, self.LightsOn, button)
+
+        panel.Add(button, 0, wx.ALL, 10)
+
 
         panel.SetSizer(box)
         panel.Layout()
@@ -32,6 +39,12 @@ class FullFrame(wx.Frame):
         self._bridge = Bridge(ip = '192.168.0.18', username = "149171db2cd4b12f26cc0ce3336b36b7")
         self._bridge.set_light( light_id = 2, parameter = {on: False})
         self.Close(True)
+
+    def LightsOff(self, event):
+        self._bridge = Bridge(ip = '192.168.0.18', username = "149171db2cd4b12f26cc0ce3336b36b7")
+        self._bridge.set_light( light_id = 2, parameter = {on: True})
+        self.Close(True)
+
 
 
 import logging
